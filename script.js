ddbb37b43fe83db2cb40a86ab2b31fb1081f94c7
João Marcos Chaves <102380570+Joaomarcos7@users.noreply.github.com>
2022-10-21 19:21:42 -0300

    Tratamento de exceções

--- script.js
+++ script.js
@@ -5,16 +5,17 @@
  let rua=document.getElementById('rua');
  let cidade=document.getElementById('cidade');
  let estado=document.getElementById('estado');
+ let error=document.getElementById('error');
 
 
- numbers.value=''
- bairro.value=''
- rua.value=''
- cidade.value=''
- estado.value=''
+ 
  
 
 async function getcep(cep){
+    bairro.value=''
+    rua.value=''
+    cidade.value=''
+    estado.value=''
 if(cep.length === 8){
 const url= 'https://viacep.com.br/ws/' + cep + '/json/'
  const response = await fetch(url)
@@ -24,11 +25,25 @@ bairro.value=info.bairro
 rua.value=info.logradouro
 cidade.value=info.localidade
 estado.value=info.uf
-
+error.style.display='none';
+  numbers.style.background='';
+  numbers.style.border='';
+if (info.erro==true){
+    error.style.display='block';
+    numbers.style.background='pink';
+    numbers.style.border='1px solid red';
+    error.innerHTML='Esse Cep não existe!';
+    bairro.value=''
+    rua.value=''
+    cidade.value=''
+    estado.value=''
+  }
 }
 else{
-    console.log('Seu CEP é inválido!')
-    alert('Seu CEP é inválido!')
+  error.style.display='block';
+  error.innerHTML='Cep inválido!';
+  numbers.style.background='pink';
+  numbers.style.border='1px solid red';
 }
 }
 
